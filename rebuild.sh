c05400caa642d4bda55f997e0456b52fec328200
vijayakumar.jawaharlal@gmail.com <vijayakumar.jawaharlal@gmail.com>
2015-06-16 08:23:17 -0400

    Docker file changed to get storm 0.9.5 version

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="njvijay/storm:0.9.5" storm
+docker build -t="njvijay/storm-nimbus" storm-nimbus
+docker build -t="njvijay/storm-supervisor" storm-supervisor
+docker build -t="njvijay/storm-ui" storm-ui
